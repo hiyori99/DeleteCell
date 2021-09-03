53fb68f9227c1175d9cb889667eabbde201c6ff1
hiyori99 <luluikdkoko@gmail.com>
2021-09-03 23:35:57 +0900

    Initial Commit

---
+++ DeleteCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeleteCell
+//
+//  Created by 池田光里 on 2021/09/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
